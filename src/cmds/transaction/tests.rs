f718510124f08364da0f44bc8fd76cf0fa854ebe
Robert Klotzner <robert.klotzner@gmx.at>
2020-02-24 22:18:21 +0100

    Fix tests

--- src/cmds/transaction/tests.rs
+++ src/cmds/transaction/tests.rs
@@ -1,66 +1,48 @@
 use rust_decimal::Decimal;
+use chrono::naive::NaiveDate;
+use indoc::indoc;
 
-use crate::transaction::templates::Templates;
-use crate::transaction::Transaction;
-use super::Message;
-
+use super::{*};
 
 #[test]
 fn default_expenses_is_food() {
-    let ts = Templates::new();
-    let tr = Transaction::parse_message(&ts, "");
-    assert_eq!(&tr.to_posting.account, "expenses:food");
+    let tr = mk_transaction("slkdjfsl 2,30");
+    assert_eq!(&tr.to_account, "expenses:food");
 }
 
 #[test]
 fn default_assets_is_cash() {
-    let ts = Templates::new();
-    let tr = Transaction::parse_message(&ts, "");
-    assert_eq!(&tr.from_posting.account, "assets:cash");
+    let tr = mk_transaction("sdkajskl 2");
+    assert_eq!(&tr.from_account, "assets:cash");
 }
 
 #[test]
 fn assets_can_be_abbreviated() {
-    let ts = Templates::new();
-    let tr = Transaction::parse_message(&ts, "stuff 3.40 bank-account");
-    assert_eq!(&tr.from_posting.account, "assets:bank-account");
+    let tr = mk_transaction("stuff 3.40 bank-account");
+    assert_eq!(&tr.from_account, "assets:bank-account");
 }
 
 #[test]
 fn expenses_can_be_abbreviated() {
-    let ts = Templates::new();
-    let tr = Transaction::parse_message(&ts, "stuff 3.40 bank-account life");
-    assert_eq!(&tr.to_posting.account, "expenses:life");
+    let tr = mk_transaction("stuff 3.40 bank-account life");
+    assert_eq!(&tr.to_account, "expenses:life");
 }
 
 #[test]
 fn german_comma_is_accepted() {
-    let ts = Templates::new();
-    let tr = Transaction::parse_message(&ts, "stuff 3,40 bank-account life");
-    assert_eq!(tr.from_posting.amount, -Decimal::new(340, 2));
+    let tr = mk_transaction("stuff 3,40 bank-account life");
+    assert_eq!(tr.amount, Decimal::new(340, 2));
 }
 
-#[test]
-fn amount_is_negated() {
-    let ts = Templates::new();
-    let tr = Transaction::parse_message(&ts, "stuff 3.40 bank-account life");
-    assert_eq!(tr.from_posting.amount, -Decimal::new(340, 2));
-    assert_eq!(tr.to_posting.amount, Decimal::new(340, 2));
-}
 
 #[test]
 fn transaction_string_has_expected_format() {
     let t = Transaction {
         description: String::from("Test"),
         date: NaiveDate::from_ymd(2020, 02, 23),
-        from_posting: Posting {
-            account: "assets:giro".to_string(),
-            amount: Decimal::new(-314, 2),
-        },
-        to_posting: Posting {
-            account: "expenses:other".to_string(),
-            amount: Decimal::new(314, 2),
-        },
+        amount: Decimal::new(314, 2),
+        from_account: "assets:giro".to_string(),
+        to_account: "expenses:other".to_string(),
     };
     let expected_output = indoc!("
         2020/02/23   Test
@@ -69,4 +51,8 @@ fn transaction_string_has_expected_format() {
 
         ");
     assert_eq!(format!("{}", t), expected_output);
+}
+
+fn mk_transaction(msg: &str) -> Transaction {
+    Transaction::try_from(Message::parse(msg)).expect("Invalid Transaction")
 }
